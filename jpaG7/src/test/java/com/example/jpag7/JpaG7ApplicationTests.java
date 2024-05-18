ecee223b26251e80a39c513ab5f3a463df9fbc77
Lupo_YA <151574836+Ya3h1ya@users.noreply.github.com>
2024-05-18 15:08:45 +0100

    Add files via upload

---
+++ jpaG7/src/test/java/com/example/jpag7/JpaG7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpag7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaG7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
